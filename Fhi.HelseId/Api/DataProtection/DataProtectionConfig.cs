efbef9cf0ac645875b78a8dfbb0e10ba410d0e10
Terje Sandstrøm <terje@hermit.no>
2020-10-22 17:49:25 +0200

    Lagt til dataprotection extensions for sql  #3

---
+++ Fhi.HelseId/Api/DataProtection/DataProtectionConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Fhi.HelseId.Api.DataProtection
+{
+    public class DataProtectionConfig
+    {
+        public bool Enabled { get; set; }
+        public string ConnectionString { get; set; } = "";
+        public string Schema { get; set; } = "DataProtection";
+        public string TableName { get; set; } = "Keys";
+    }
+}
