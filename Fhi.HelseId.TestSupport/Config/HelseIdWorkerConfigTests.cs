a175665055dd3f18381cbb853cb4966a7d17e850
Terje Sandstrom <terje@hermit.no>
2021-12-20 15:22:45 +0100

    added testsupport

--- Fhi.HelseId.TestSupport/Config/HelseIdWorkerConfigTests.cs
+++ Fhi.HelseId.TestSupport/Config/HelseIdWorkerConfigTests.cs
@@ -5,7 +5,45 @@ using NUnit.Framework;
 
 namespace Fhi.HelseId.TestSupport.Config
 {
-    public abstract class HelseIdWorkerConfigTests : BaseConfigTests
+
+    public abstract class HelseIdClientBased : BaseConfigTests
+    {
+        protected HelseIdClientBased(string configFile, bool test) : base(configFile, test)
+        {
+        }
+
+        protected abstract HelseIdClientKonfigurasjon HelseIdClientKonfigurasjon { get;}
+
+        [Test]
+        public void ThatClientIdIsSet()
+        {
+            Guard();
+            Assert.That(HelseIdClientKonfigurasjon.ClientId.Length, Is.GreaterThan(0), $"{ConfigFile}: ClientId is not set");
+            TestContext.Out.WriteLine($"ClientId is: {HelseIdClientKonfigurasjon.ClientId}");
+        }
+
+        [Test]
+        public void ThatScopesDontExceedLimit()
+        {
+            const int maxScopeLengthInHelseId = 600;
+            Guard();
+            string scopes = string.Join(',', HelseIdClientKonfigurasjon.AllScopes);
+            Assert.That(scopes.Length, Is.LessThanOrEqualTo(maxScopeLengthInHelseId), $"{ConfigFile}: Combined scopes have a maximum length of 600 - including separators, this is {scopes.Length}");
+            TestContext.Out.WriteLine($"{ConfigFile}: Total length of scopes is {scopes.Length}");
+        }
+
+        [Test]
+        public void ThatAuthUseIsTrue()
+        {
+            Guard();
+            Assert.That(HelseIdClientKonfigurasjon.AuthUse, $"{ConfigFile} AuthUse should be true, but is false");
+        }
+
+    }
+
+
+
+    public abstract class HelseIdWorkerConfigTests : HelseIdClientBased
     {
         protected HelseIdWorkerConfigTests(string file, bool test) : base(file, test)
         {
@@ -16,16 +54,9 @@ namespace Fhi.HelseId.TestSupport.Config
 
         protected override HelseIdCommonKonfigurasjon HelseIdKonfigurasjonUnderTest => HelseIdWorkerKonfigurasjonUnderTest;
 
+        protected override HelseIdClientKonfigurasjon HelseIdClientKonfigurasjon => HelseIdWorkerKonfigurasjonUnderTest;
 
-        [Test]
-        public void ThatScopesDontExceedLimit()
-        {
-            const int maxScopeLengthInHelseId = 600;
-            Guard();
-            string scopes = string.Join(',', HelseIdWorkerKonfigurasjonUnderTest.AllScopes);
-            Assert.That(scopes.Length, Is.LessThanOrEqualTo(maxScopeLengthInHelseId), $"{ConfigFile}: Combined scopes have a maximum length of 600 - including separators, this is {scopes.Length}");
-            TestContext.Out.WriteLine($"{ConfigFile}: Total length of scopes is {scopes.Length}");
-        }
+        
 
         [Test]
         public void ThatAuthorityHasTokenSuffix()
@@ -35,25 +66,11 @@ namespace Fhi.HelseId.TestSupport.Config
                 $"{ConfigFile}: An Worker configuration should have the authority url with suffix 'connect/token', but is {HelseIdWorkerKonfigurasjonUnderTest.Authority}.");
         }
 
-        [Test]
-        public void ThatClientIdIsSet()
-        {
-            Guard();
-            Assert.That(HelseIdWorkerKonfigurasjonUnderTest.ClientId.Length, Is.GreaterThan(0), $"{ConfigFile}: ClientId is not set");
-            TestContext.Out.WriteLine($"ClientId is: {HelseIdWorkerKonfigurasjonUnderTest.ClientId}");
-        }
-
-        [Test]
-        public void ThatAuthUseIsTrue()
-        {
-            Guard();
-            Assert.That(HelseIdWorkerKonfigurasjonUnderTest.AuthUse, $"{ConfigFile} AuthUse should be true, but is false");
-        }
-
-
         protected override void Guard()
         {
             Assert.That(HelseIdWorkerKonfigurasjonUnderTest, Is.Not.Null, $"{ConfigFile}: No config section named 'HelseIdWorkerKonfigurasjon' found, or derived");
         }
     }
+
+
 }
