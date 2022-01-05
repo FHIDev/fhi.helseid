48b519c09a7752d1bc9d46b46b5e84c7e962ddae
Tor Anders Johansen <mail@toranders.com>
2022-01-05 20:33:26 +0100

    Allow to configure Cookie- and OpenIdConnect Authentication (#121)
    * Allow to configure Cookie- and OpenIdConnect Authentication
    
    * Common class for all ConfigureAuthentication-actions
    
    Co-authored-by: Tor Anders Johansen <TorAnders.Johansen@fhi.no>

---
+++ Fhi.HelseId/Api/ConfigureAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Fhi.HelseId.Api
+{
+    public class ConfigureAuthentication
+    {
+        public Action<CookieAuthenticationOptions>? ConfigureCookieAuthentication { get; set; }
+        public Action<OpenIdConnectOptions>? ConfigureOpenIdConnect { get; set; }
+    }
+}
