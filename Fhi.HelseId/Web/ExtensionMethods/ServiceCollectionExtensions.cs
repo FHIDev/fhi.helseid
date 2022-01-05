146a6247d67418531c4195c135bbf88e38f86884
Tor Anders Johansen <mail@toranders.com>
2022-01-05 19:37:48 +0100

    Added RewriteRedirectUriHttps-config to rewrite Redirect Uri (#120)
    * Added RewriteRedirectUriHttps-config to rewrite Redirect Uri to use https
    
    * Added RewriteRedirectUriHttps-property to HelseIdClientKonfigurasjon.cs
    
    * Fix possible null-pointer-exception in HelseIdWorkerConfigurationTest-unittest
    
    Co-authored-by: Tor Anders Johansen <TorAnders.Johansen@fhi.no>

--- Fhi.HelseId/Web/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Fhi.HelseId/Web/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -143,7 +143,7 @@ namespace Fhi.HelseId.Web.ExtensionMethods
                     services.AddScoped<IAuthorizationHandler, HprGodkjenningAuthorizationHandler>();
             }
 
-            return (policyName, mvcBuilder);;            
+            return (policyName, mvcBuilder);        
         }
 
         public static IMvcBuilder AddHelseIdWebAuthentication(this IServiceCollection services,
