2b3c1e74a3186ca4006d59c1031df4f7c9c61627
Terje Sandstrom <terje@hermit.no>
2022-05-04 12:05:37 +0200

    Cleanup

--- Fhi.HelseId.Worker.Tests/HelseIdWorkerConfigurationTests.cs
+++ Fhi.HelseId.Worker.Tests/HelseIdWorkerConfigurationTests.cs
@@ -14,14 +14,14 @@ namespace Fhi.HelseId.Worker.Tests
             Assert.That(sut, Is.Not.Null, $"Can't load {ConfigFilename}");
             Assert.Multiple(() =>
             {
-                Assert.That(sut?.Authority, Does.Contain("helseid-sts"), "1");
-                Assert.That(sut?.AuthUse, Is.True, "2");
-                Assert.That(sut?.UseHttps, Is.True, "3");
-                Assert.That(sut?.ClientId.Length, Is.GreaterThan(1));
-                Assert.That(sut?.AllScopes.Count, Is.GreaterThanOrEqualTo(1), "4");
-                Assert.That(sut?.Apis.Length, Is.EqualTo(1), "5");
+                Assert.That(sut.Authority, Does.Contain("helseid-sts"), "1");
+                Assert.That(sut.AuthUse, Is.True, "2");
+                Assert.That(sut.UseHttps, Is.True, "3");
+                Assert.That(sut.ClientId.Length, Is.GreaterThan(1));
+                Assert.That(sut.AllScopes.Count, Is.GreaterThanOrEqualTo(1), "4");
+                Assert.That(sut.Apis.Length, Is.EqualTo(1), "5");
             });
-            var api = sut?.Apis[0];
+            var api = sut.Apis[0];
             Assert.Multiple(() =>
             {
                 Assert.That(api.Name, Is.EqualTo("Personoppslag"));
