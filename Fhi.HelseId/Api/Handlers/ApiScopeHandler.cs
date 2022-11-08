dd9e1960539a416865eb7d8b7702f56787a922cc
Terje Sandstrom <terje@hermit.no>
2022-11-08 18:08:40 +0100

    Lagt til api  auth handler logging

--- Fhi.HelseId/Api/Handlers/ApiScopeHandler.cs
+++ Fhi.HelseId/Api/Handlers/ApiScopeHandler.cs
@@ -3,16 +3,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Fhi.HelseId.Common;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
 
 namespace Fhi.HelseId.Api.Handlers
 {
     public class ApiScopeHandler : AuthorizationHandler<SecurityLevelOrApiRequirement>
     {
         private readonly IHelseIdApiKonfigurasjon _configAuth;
+        private readonly ILogger<ApiScopeHandler> _logger;
 
-        public ApiScopeHandler(IHelseIdApiKonfigurasjon configAuth)
+        public ApiScopeHandler(IHelseIdApiKonfigurasjon configAuth, ILogger<ApiScopeHandler> logger)
         {
             _configAuth = configAuth;
+            _logger = logger;
         }
 
         protected override Task HandleRequirementAsync(
@@ -20,9 +23,14 @@ namespace Fhi.HelseId.Api.Handlers
         {
             var scopeClaims = context.User.FindAll("scope");
 
-            if(scopeClaims.Any(c => StringComparer.InvariantCultureIgnoreCase.Equals(c.Value, _configAuth.ApiScope)))
+            if (scopeClaims.Any(c => StringComparer.InvariantCultureIgnoreCase.Equals(c.Value, _configAuth.ApiScope)))
             {
                 context.Succeed(requirement);
+            }
+            else
+            {
+                string scopeHandler = nameof(ApiScopeHandler);
+                _logger.LogError("Fhi.HelseId.Api.Handlers.{scopeHandler}: Missing or invalid scope ({scopeClaims}), access denied", scopeClaims,scopeHandler);
             }
 
             return Task.CompletedTask;
