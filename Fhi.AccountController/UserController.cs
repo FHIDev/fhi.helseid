d14c995954d9af803147f61d3d9b936041442af4
Terje Sandstrom <terje@hermit.no>
2023-04-20 12:00:44 +0200

    Updating accoutncontroller with user.

---
+++ Fhi.AccountController/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fhi.HelseId.Web.Services;
+
+namespace Fhi.AuthControllers
+{
+
+    [Route("User")]
+    [ApiController]
+    [AllowAnonymous]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> logger;
+        private readonly ICurrentUser currentUser;
+
+        public UserController(ILogger<UserController> logger, ICurrentUser currentUser)
+        {
+            this.logger = logger;
+            this.currentUser = currentUser;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<ICurrentUser>> GetUser()
+        {
+            return Ok(currentUser);
+        }
+    }
+}
