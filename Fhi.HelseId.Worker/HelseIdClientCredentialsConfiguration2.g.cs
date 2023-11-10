044b46d892b54bb032bc4a1301a1387ac839ecd6
Terje Sandstrom <terje@hermit.no>
2023-11-10 10:45:49 +0100

    updated version to 5.0.0, deleted worker, and upgraded packages

--- Fhi.HelseId.Worker/HelseIdClientCredentialsConfiguration2.g.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace Fhi.HelseId.Worker;
-
-public partial class HelseIdClientCredentialsConfiguration
-{
-	public string url { get; set; }
-}
-
--- Fhi.HelseId.Worker/copynp.cmd
+++
@@ -1,2 +0,0 @@
-copy bin\debug\Fhi.HelseId.Worker.%1.nupkg  c:\nuget
-copy bin\debug\Fhi.HelseId.Worker.%1.snupkg  c:\nuget
