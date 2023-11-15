0f88405acd7548cbf1240ba88a93dbcecc4e2356
Terje Sandstrom <terje@hermit.no>
2023-11-15 11:04:08 +0100

    Remove revoke of refreshtoken (#283)
    * Remove revoke of refreshtoken
    
    * Added config for refreshtokenstore enable/disable.  If refreshtoken is set to ReUse, set this to false.  Currently default is true.

--- Fhi.HelseId/Web/Infrastructure/AutomaticTokenManagement/AutomaticTokenManagementOptions.cs
+++ Fhi.HelseId/Web/Infrastructure/AutomaticTokenManagement/AutomaticTokenManagementOptions.cs
@@ -7,11 +7,11 @@ namespace Fhi.HelseId.Web.Infrastructure.AutomaticTokenManagement
     {
         public string? Scheme { get; set; }
         public TimeSpan RefreshBeforeExpiration { get; set; } = TimeSpan.FromMinutes(1);
-        public bool RevokeRefreshTokenOnSignout { get; set; } = true;
+        public bool RevokeRefreshTokenOnSignout { get; set; } = false;
 
         /// <summary>
         /// Event callback that will be called for the cookie this automatic token management is applied to.
         /// </summary>
-        public CookieAuthenticationEvents CookieEvents { get; set; } = new CookieAuthenticationEvents();
+        public CookieAuthenticationEvents CookieEvents { get; set; } = new();
     }
 }
