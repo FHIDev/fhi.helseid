3632b3441c6da3133c9c540dbc6564f0aeec4144
Terje Sandstrom <terje@hermit.no>
2023-11-22 18:10:39 +0100

    Oppdatert ApiBuilder

--- Fhi.AccountController/UserController.cs
+++ Fhi.AccountController/UserController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Fhi.HelseId.Web.Services;
 
 namespace Fhi.AuthControllers
