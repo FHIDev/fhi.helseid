433e50031bc3086af885cc8a050a08fbbe7e1ce1
Terje Sandstrom <terje@hermit.no>
2023-12-01 19:35:39 +0100

    520beta1 (#310)
    * Update to version 5.2.0-beta.1
    
    * Updating api in HelseId

--- Fhi.HelseId/Api/Services/CurrentUser.cs
+++ Fhi.HelseId/Api/Services/CurrentUser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Fhi.HelseId.Common.Identity;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 
 namespace Fhi.HelseId.Api.Services;
@@ -16,30 +17,40 @@ public interface ICurrentUser
     string? Pid { get; }
     public string? PidPseudonym { get; }
     public IEnumerable<string> Scopes { get; }
+    string Fornavn { get; set; }
+    string Etternavn { get; set; }
 }
 
 /// <summary>
-/// For Api access only
+/// For Api access only. For web backends use CurrentWebUser
+/// Ensure your config specifies "RequireContextIdentity" : true
 /// </summary>
 public class CurrentHttpUser : ICurrentUser
 {
     public CurrentHttpUser(IHttpContextAccessor httpContextAccessor)
     {
         var httpContext = httpContextAccessor.HttpContext!;
-        Id = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
+        Id = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value ?? "";
         HprNummer = httpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimsPrincipalExtensions.HprNummer)
-            ?.Value;
-        Name = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Name)?.Value;
-        Pid = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
-        PidPseudonym = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.PidPseudonym)?.Value;
+            ?.Value ?? "";
+        Fornavn= httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Givenname)?.Value??"";
+        Etternavn = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Familyname)?.Value??"";
+        Pid = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value ?? "";
+        PidPseudonym = httpContext.User.Claims.FirstOrDefault(x => x.Type == IdentityClaims.PidPseudonym)?.Value??"";
         Scopes = httpContext.User.FindAll("scope").Select(o => o.Value).ToList();
+        ClientName = httpContext.User.Claims.FirstOrDefault(x => x.Type == ClientClaims.ClientName)?.Value??"";
     }
 
-    public string? Id { get; }
-    public string? HprNummer { get; }
-    public string? Name { get; }
-    public string? Pid { get; }
-    public string? PidPseudonym { get; }
+    public string Id { get; }
+    public string HprNummer { get; }
+    public string Name => $"{Fornavn} {Etternavn}";
+    public string Pid { get; }
+    public string PidPseudonym { get; } 
     public IEnumerable<string> Scopes { get; }
+
+    public string Fornavn { get; set; }
+    public string Etternavn { get; set; }
+
+    public string ClientName { get; set; }
 
 }
--- Fhi.HelseId/Identity/HprClaims.cs
+++ Fhi.HelseId/Identity/HprClaims.cs
@@ -1,28 +1,27 @@
 ﻿using System.Linq;
 using System.Security.Claims;
 
-namespace Fhi.HelseId.Common.Identity
-{
-    public static class ClaimsPrincipalExtensions
-    {
-        private const string Prefix = HelseIdUriPrefixes.Claims + "hpr/";
-        public const string HprNummer = Prefix + "hpr_number";
+namespace Fhi.HelseId.Common.Identity;
 
-        public static string? HprNumber(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == HprNummer)?.Value;
-        public static string? Id(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
-        public static string? Name(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Name)?.Value;
-        public static string? PidPseudonym(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.PidPseudonym)?.Value;
-        public static string? Pid(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
-        public static string? SecurityLevel(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.SecurityLevel)?.Value;
-        public static string? AssuranceLevel(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.AssuranceLevel)?.Value;
-        public static string? Network(this ClaimsPrincipal user) =>
-            user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Network)?.Value;
-    }
+public static class ClaimsPrincipalExtensions
+{
+    private const string Prefix = HelseIdUriPrefixes.Claims + "hpr/";
+    public const string HprNummer = Prefix + "hpr_number";
+
+    public static string? HprNumber(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == HprNummer)?.Value;
+    public static string? Id(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
+    public static string? Name(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Name)?.Value;
+    public static string? PidPseudonym(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.PidPseudonym)?.Value;
+    public static string? Pid(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Pid)?.Value;
+    public static string? SecurityLevel(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.SecurityLevel)?.Value;
+    public static string? AssuranceLevel(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.AssuranceLevel)?.Value;
+    public static string? Network(this ClaimsPrincipal user) =>
+        user.Claims.FirstOrDefault(x => x.Type == IdentityClaims.Network)?.Value;
 }
