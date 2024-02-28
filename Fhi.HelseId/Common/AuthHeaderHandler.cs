8c55fdec4d0a990993fa6a78aa0112b5ed2bb6ca
Terje Sandstrom <terje@hermit.no>
2024-02-28 19:36:37 +0100

    Packages upgraded (#362)
    * Updated packages
    
    * upgr version to 5.4.3
    
    * Fixing warnings

--- Fhi.HelseId/Common/AuthHeaderHandler.cs
+++ Fhi.HelseId/Common/AuthHeaderHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Fhi.HelseId.Web;
@@ -14,106 +13,99 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Fhi.HelseId.Common
+namespace Fhi.HelseId.Common;
+
+/// <summary>
+/// This is to be used with either User or Client credentials
+/// See https://github.com/reactiveui/refit#bearer-authentication  
+/// </summary>
+public class AuthHeaderHandler : DelegatingHandler
 {
-    /// <summary>
-    /// This is to be used with either User or Client credentials
-    /// See https://github.com/reactiveui/refit#bearer-authentication  
-    /// </summary>
-    public class AuthHeaderHandler : DelegatingHandler
+    private readonly IHttpContextAccessor contextAccessor;
+    private readonly ILogger<AuthHeaderHandler> logger;
+    private readonly IRefreshTokenStore refreshTokenStore;
+    private readonly ICurrentUser user;
+    private readonly HelseIdWebKonfigurasjon config;
+
+    public AuthHeaderHandler(IHttpContextAccessor contextAccessor,
+        ILogger<AuthHeaderHandler> logger,
+        IRefreshTokenStore refreshTokenStore,
+        ICurrentUser user,
+        IOptions<HelseIdWebKonfigurasjon> options)
     {
-        private readonly IHttpContextAccessor contextAccessor;
-        private readonly ILogger<AuthHeaderHandler> logger;
-        private readonly IRefreshTokenStore refreshTokenStore;
-        private readonly ICurrentUser user;
-        private readonly HelseIdWebKonfigurasjon config;
+        config = options.Value;
+        logger.LogMember();
+        this.contextAccessor = contextAccessor;
+        this.logger = logger;
+        this.refreshTokenStore = refreshTokenStore;
+        this.user = user;
+    }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var ctx = contextAccessor.HttpContext ?? throw new NoContextException();
+        logger.LogTrace("{class}.{method} - Starting", nameof(AuthHeaderHandler), nameof(SendAsync));
+        var token = await ctx.GetUserAccessTokenAsync(cancellationToken: cancellationToken);
 
-        public AuthHeaderHandler(IHttpContextAccessor contextAccessor,
-                                 ILogger<AuthHeaderHandler> logger,
-                                 IRefreshTokenStore refreshTokenStore,
-                                 ICurrentUser user,
-                                 IOptions<HelseIdWebKonfigurasjon> options)
+        if (config.UseRefreshTokenStore && refreshTokenStore.GetLatestToken(user)!=null && !string.IsNullOrEmpty(refreshTokenStore.GetLatestToken(user)?.AccessToken) && refreshTokenStore.GetLatestToken(user)?.AccessToken != token)
+            token = refreshTokenStore.GetLatestToken(user)?.AccessToken;
+        if (token == null)
         {
-            config = options.Value;
-            logger.LogMember();
-            this.contextAccessor = contextAccessor;
-            this.logger = logger;
-            this.refreshTokenStore = refreshTokenStore;
-            this.user = user;
+            logger.LogError("{class}.{method} No access token found in context. Make sure you have added the AddTokenManagement() to your Startup.cs", nameof(AuthHeaderHandler), nameof(SendAsync));
         }
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        else
         {
-            var ctx = contextAccessor.HttpContext ?? throw new NoContextException();
-            logger.LogTrace("{class}.{method} - Starting", nameof(AuthHeaderHandler), nameof(SendAsync));
-            var token = await ctx.GetUserAccessTokenAsync(cancellationToken: cancellationToken);
-
-            if (config.UseRefreshTokenStore && refreshTokenStore.GetLatestToken(user)!=null && !string.IsNullOrEmpty(refreshTokenStore.GetLatestToken(user)?.AccessToken) && refreshTokenStore.GetLatestToken(user)?.AccessToken != token)
-                token = refreshTokenStore.GetLatestToken(user)?.AccessToken;
-            if (token == null)
-            {
-                logger.LogError("{class}.{method} No access token found in context. Make sure you have added the AddTokenManagement() to your Startup.cs", nameof(AuthHeaderHandler), nameof(SendAsync));
-            }
-            else
-            {
-               logger.LogTrace("{class}.{method} - Found access token in context (hash:{hash})", nameof(AuthHeaderHandler), nameof(SendAsync),token.GetHashCode());
-            }
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-            if (!response.IsSuccessStatusCode)
-            {
-                logger.LogError("{class}.{method} Request to {url} failed with status code {statusCode}", nameof(AuthHeaderHandler), nameof(SendAsync),request.RequestUri, response.StatusCode);
-            }
-
-            return response;
+            logger.LogTrace("{class}.{method} - Found access token in context (hash:{hash})", nameof(AuthHeaderHandler), nameof(SendAsync),token.GetHashCode());
         }
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        if (!response.IsSuccessStatusCode)
+        {
+            logger.LogError("{class}.{method} Request to {url} failed with status code {statusCode}", nameof(AuthHeaderHandler), nameof(SendAsync),request.RequestUri, response.StatusCode);
+        }
+
+        return response;
+    }
+}
+
+/// <summary>
+/// This is to be used for Apis that need to send same access tokens onwards to further Apis
+/// See https://github.com/reactiveui/refit#bearer-authentication
+/// </summary>
+public class AuthHeaderHandlerForApi : DelegatingHandler
+{
+    private readonly IHttpContextAccessor contextAccessor;
+    public AuthHeaderHandlerForApi(IHttpContextAccessor contextAccessor)
+    {
+        this.contextAccessor = contextAccessor;
+    }
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var ctx = contextAccessor.HttpContext ?? throw new NoContextException();
+        var token = await ctx.AccessToken();
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",  token);
+        return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+    }
+}
+
+public class NoContextException: Exception
+{
+    //
+    // For guidelines regarding the creation of new exception types, see
+    //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+    // and
+    //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+    //
+
+    public NoContextException()
+    {
     }
 
-    /// <summary>
-    /// This is to be used for Apis that need to send same access tokens onwards to further Apis
-    /// See https://github.com/reactiveui/refit#bearer-authentication
-    /// </summary>
-    public class AuthHeaderHandlerForApi : DelegatingHandler
+    public NoContextException(string message) : base(message)
     {
-        private readonly IHttpContextAccessor contextAccessor;
-        public AuthHeaderHandlerForApi(IHttpContextAccessor contextAccessor)
-        {
-            this.contextAccessor = contextAccessor;
-        }
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            var ctx = contextAccessor.HttpContext ?? throw new NoContextException();
-            var token = await ctx.AccessToken();
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",  token);
-            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-        }
     }
 
-    [Serializable]
-    public class NoContextException: Exception
+    public NoContextException(string message, Exception inner) : base(message, inner)
     {
-        //
-        // For guidelines regarding the creation of new exception types, see
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
-        // and
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
-        //
-
-        public NoContextException()
-        {
-        }
-
-        public NoContextException(string message) : base(message)
-        {
-        }
-
-        public NoContextException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected NoContextException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
-        {
-        }
     }
+
 }
--- Fhi.HelseId/Web/Infrastructure/TokenExtensions.cs
+++ Fhi.HelseId/Web/Infrastructure/TokenExtensions.cs
@@ -1,64 +1,51 @@
 ﻿using System;
-using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authentication;
-using Azure;
 
-namespace Fhi.HelseId.Web.Infrastructure
+namespace Fhi.HelseId.Web.Infrastructure;
+
+public static class TokenExtensions
 {
-    public static class TokenExtensions
+    public static async Task<string> AccessToken(this HttpContext ctx)
     {
-        public static async  Task<string> AccessToken(this HttpContext ctx)
-        {
-            var ret =  await ctx.GetTokenAsync("access_token");
-            return ret ?? throw new NoTokenException("Missing access token");
-        }
+        var ret = await ctx.GetTokenAsync("access_token");
+        return ret ?? throw new NoTokenException("Missing access token");
+    }
 
-        public static async Task<string> IdentityToken(this HttpContext ctx)
-        {
-            var ret =  await ctx.GetTokenAsync("id_token");
-            return ret ?? throw new NoTokenException("Missing identity token");
-        }
+    public static async Task<string> IdentityToken(this HttpContext ctx)
+    {
+        var ret = await ctx.GetTokenAsync("id_token");
+        return ret ?? throw new NoTokenException("Missing identity token");
+    }
 
-        public static async Task<string> RefreshToken(this HttpContext ctx)
-        {
-            var ret = await ctx.GetTokenAsync("refresh_token");
-            return ret ?? throw new NoTokenException("Missing refresh token");
-        }
-
-        
+    public static async Task<string> RefreshToken(this HttpContext ctx)
+    {
+        var ret = await ctx.GetTokenAsync("refresh_token");
+        return ret ?? throw new NoTokenException("Missing refresh token");
     }
 
 
+}
 
+public class NoTokenException : Exception
+{
+    //
+    // For guidelines regarding the creation of new exception types, see
+    //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+    // and
+    //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+    //
 
-    [Serializable]
-    public class NoTokenException : Exception
+    public NoTokenException()
     {
-        //
-        // For guidelines regarding the creation of new exception types, see
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
-        // and
-        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
-        //
+    }
 
-        public NoTokenException()
-        {
-        }
+    public NoTokenException(string message) : base(message)
+    {
+    }
 
-        public NoTokenException(string message) : base(message)
-        {
-        }
-
-        public NoTokenException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected NoTokenException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
-        {
-        }
+    public NoTokenException(string message, Exception inner) : base(message, inner)
+    {
     }
 }
