37fe33a52d3f809eb644d8889b2f59521d2cb31b
Martin Storø Nyfløtt <MartinStoro.Nyflott@fhi.no>
2024-04-19 15:43:44 +0200

    Support for using HelseId in Swagger

---
+++ Fhi.HelseId.Swagger/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Fhi.HelseId.Swagger;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddHelseIdTestTokens(this WebApplicationBuilder builder, SwaggerHelseIdConfiguration swaggerAuthConfig)
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddSingleton(swaggerAuthConfig);
+            builder.Services.AddSingleton<ITokenProxy, TokenProxy>();
+        }
+
+        return builder;
+    }
+}
---
+++ Fhi.HelseId.Swagger/WebApplicationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Net.Mime;
+
+namespace Fhi.HelseId.Swagger;
+
+internal static class WebApplicationExtensions
+{
+    public static RouteHandlerBuilder UseHelseIdTestTokenEndpoint(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            return app.MapPost(".swagger-dev/token", async Task<IResult> ([FromForm] IFormCollection form, ITokenProxy tokenProxy) =>
+            {
+                var requestParameters = form.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First());
+                var jsonResult = await tokenProxy.RequestToken(requestParameters);
+
+                return Results.Text(jsonResult, MediaTypeNames.Application.Json);
+            })
+            .DisableAntiforgery()
+            .ExcludeFromDescription();
+        }
+
+        return new RouteHandlerBuilder([]);
+    }
+}
