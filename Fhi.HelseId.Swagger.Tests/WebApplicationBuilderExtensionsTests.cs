7e1ac3a2b6d8d79b1eceba4c4ee36e5b11afb1df
Martin Storø Nyfløtt <martinmine@users.noreply.github.com>
2024-05-06 10:55:23 +0200

    Support for HelseID token generation in Swagger (#378)
    * Support for using HelseId in Swagger
    
    * Minor fixes
    
    * Justere pakke-metadata
    
    * Update Fhi.HelseId.Swagger/README.md
    
    Co-authored-by: Terje Sandstrom <terje@hermit.no>
    
    * Update Fhi.HelseId.Swagger/README.md
    
    Co-authored-by: Terje Sandstrom <terje@hermit.no>
    
    * Allow specifying enabled environments
    
    * Refactoring and added unit tests
    
    ---------
    
    Co-authored-by: Martin Storø Nyfløtt <MartinStoro.Nyflott@fhi.no>
    Co-authored-by: Terje Sandstrom <terje@hermit.no>

---
+++ Fhi.HelseId.Swagger.Tests/WebApplicationBuilderExtensionsTests.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fhi.HelseId.Swagger.Tests;
+
+public class WebApplicationBuilderExtensionsTests
+{
+    [TestCase("Prod")]
+    [TestCase("Production")]
+    public void SwaggerTokenServicesAreNotRegisteredForProdEnvironments(string environmentName)
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        builder.Environment.EnvironmentName = environmentName;
+        var swaggerHelseIdConfiguration = new SwaggerHelseIdConfiguration();
+
+        // Act
+        builder.AddHelseIdTestTokens(swaggerHelseIdConfiguration);
+
+        // Assert
+        Assert.That(builder.Services, Is.All.Not.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(ITokenProxy)));
+        Assert.That(builder.Services, Is.All.Not.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(SwaggerHelseIdConfiguration)));
+    }
+
+    [TestCase("Test")]
+    [TestCase("Stage")]
+    [TestCase("Development")]
+    public void SwaggerTokenServicesAreRegisteredForTestEnvironments(string environmentName)
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        builder.Environment.EnvironmentName = environmentName;
+        var swaggerHelseIdConfiguration = new SwaggerHelseIdConfiguration();
+
+        // Act
+        builder.AddHelseIdTestTokens(swaggerHelseIdConfiguration);
+
+        // Assert
+        Assert.That(builder.Services, Has.Some.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(ITokenProxy)));
+        Assert.That(builder.Services, Has.Some.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(SwaggerHelseIdConfiguration)));
+    }
+
+    [Test]
+    public void SwaggerTokenServicesAreRegisteredForSpecifiedEnvironments()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        builder.Environment.EnvironmentName = "SwaggerRequiredEnvironment";
+        var swaggerHelseIdConfiguration = new SwaggerHelseIdConfiguration();
+
+        // Act
+        builder.AddHelseIdTestTokens(swaggerHelseIdConfiguration, ["SwaggerRequiredEnvironment"]);
+
+        // Assert
+        Assert.That(builder.Services, Has.Some.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(ITokenProxy)));
+        Assert.That(builder.Services, Has.Some.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(SwaggerHelseIdConfiguration)));
+    }
+
+
+    [Test]
+    public void SwaggerTokenServicesAreNotRegisteredForUnspecifiedEnvironments()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        builder.Environment.EnvironmentName = "RandomEnvironment";
+        var swaggerHelseIdConfiguration = new SwaggerHelseIdConfiguration();
+
+        // Act
+        builder.AddHelseIdTestTokens(swaggerHelseIdConfiguration, ["SwaggerRequiredEnvironment"]);
+
+        // Assert
+        Assert.That(builder.Services, Is.All.Not.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(ITokenProxy)));
+        Assert.That(builder.Services, Is.All.Not.Matches<ServiceDescriptor>(sd => sd.ServiceType == typeof(SwaggerHelseIdConfiguration)));
+    }
+}
---
+++ Fhi.HelseId.Swagger/SwaggerHelseIdConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fhi.HelseId.Swagger;
+
+public class SwaggerHelseIdConfiguration
+{
+    public string TokenEndpoint { get; set; } = "";
+
+    [JsonPropertyName("clientName")]
+    public string ClientName { get; set; } = "";
+
+    [JsonPropertyName("authority")]
+    public string Authority { get; set; } = "";
+
+    [JsonPropertyName("clientId")]
+    public string ClientId { get; set; } = "";
+
+    [JsonPropertyName("scopes")]
+    public string[] Scopes { get; set; } = [];
+
+    [JsonPropertyName("privateJwk")]
+    public string PrivateJwk { get; set; } = "";
+}
