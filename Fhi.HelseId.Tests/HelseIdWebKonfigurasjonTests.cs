f5e8d7d8a1d88ab993a84752c7174e0e1a92fdde
Terje Sandstrom <terje@hermit.no>
2024-09-12 13:47:50 +0200

    FIx test

--- Fhi.HelseId.Tests/HelseIdWebKonfigurasjonTests.cs
+++ Fhi.HelseId.Tests/HelseIdWebKonfigurasjonTests.cs
@@ -20,7 +20,7 @@ namespace Fhi.HelseId.Tests
                 Assert.That(sut.AuthUse, Is.True, "2");
                 Assert.That(sut.UseHttps, Is.True, "3");
                 Assert.That(sut.UseHprNumber, Is.False, "4");
-                Assert.That(sut.AllScopes, Has.Count.GreaterThan(7), "5");
+                Assert.That(sut.AllScopes, Has.Count.GreaterThan(6), "5");
                 Assert.That(sut.Whitelist, Is.Not.Null);
                 Assert.That(sut.Whitelist, Is.Empty);
                 Assert.That(sut.Apis, Is.Not.Null);
