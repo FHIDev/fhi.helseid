56c446b7769859bdd58fe13809a2ebb675135aa1
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2024-10-14 13:52:01 +0200

    Fixes CS108 - 'HPR2ServiceClient.CloseAsync()' hides inherited member 'ClientBase<IHPR2Service>.CloseAsync()'. Use the new keyword if hiding was intended.

---
+++ Fhi.HelseId.Web/Connected Services/HprServiceReference/HprServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HprServiceReference
+{
+    internal static class HprServiceExtensions
+    {
+        internal static bool Aktiv(this Periode periode)
+            => periode.Fra < DateTime.Now && (periode.Til == null || periode.Til > DateTime.Now);
+    }
+}
