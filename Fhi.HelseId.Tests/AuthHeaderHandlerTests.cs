c7b0aeb64b1729b9b903f3dfefe4fe26bac94142
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2024-10-25 15:08:41 +0200

    Fixed failing, and false positive, tests.

--- Fhi.HelseId.Tests/AuthHeaderHandlerTests.cs
+++ Fhi.HelseId.Tests/AuthHeaderHandlerTests.cs
@@ -2,61 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using Fhi.HelseId.Common;
 using Fhi.HelseId.Web;
-using Fhi.HelseId.Web.Handlers;
 using Fhi.HelseId.Web.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using NSubstitute;
 using NUnit.Framework;
 
 namespace Fhi.HelseId.Tests;
+
 public class AuthHeaderHandlerTests
 {
-    [Test]
-    public async Task HandlerAddsAuthToken()
+    HttpClient _client = null!;
+    string _authToken = null!;
+
+    [SetUp]
+    public void SetUp()
     {
-        var authToken = Guid.NewGuid().ToString();
+        _authToken = Guid.NewGuid().ToString();
 
         var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
         var context = Substitute.For<HttpContext>();
-
         var services = new ServiceCollection();
+        var authService = Substitute.For<IAuthenticationService>();
+        var claimsPrincipal = new ClaimsPrincipal();
+        var authenticationProperties = new AuthenticationProperties();
+        authenticationProperties.StoreTokens([new AuthenticationToken
+        {
+            Name = OpenIdConnectParameterNames.AccessToken,
+            Value = _authToken
+        }]);
+        var authResult = AuthenticateResult.Success(new AuthenticationTicket(
+            claimsPrincipal,
+            authenticationProperties,
+            JwtBearerDefaults.AuthenticationScheme));
+        authService.AuthenticateAsync(Arg.Any<HttpContext>(), Arg.Any<string>()).Returns(authResult);
 
-        var logger = Substitute.For<ILoggerFactory>();
-        services.AddSingleton(logger);
+        services.AddSingleton(authService);
 
-        //services.AddAuthentication("NoAuthentication")
-        //        .AddScheme<AuthenticationSchemeOptions, NoAuthenticationHandler>("NoAuthentication", null);
-
-        services.AddHttpContextAccessor();
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = "TODO_your_issuer",
-                    ValidAudience = "TODO_your_audience",
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("TODO_your_secret_key_here")),
-                    ClockSkew = TimeSpan.Zero
-                };
-            });
-
-        context.RequestServices.Returns(services.BuildServiceProvider());
+        var serviceProvider = services.BuildServiceProvider();
+        context.RequestServices.Returns(serviceProvider);
         httpContextAccessor.HttpContext.Returns(context);
 
         var handler = new AuthHeaderHandler(
@@ -68,41 +62,34 @@ public class AuthHeaderHandlerTests
 
         handler.InnerHandler = new DummyInnerHandler();
 
-        var client = new HttpClient(handler);
+        _client = new HttpClient(handler);
+    }
+
+    [TearDown]
+    public void TearDown() => _client.Dispose();
+
+    [Test]
+    public async Task HandlerAddsAuthToken()
+    {
         var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/");
-        var response = await client.SendAsync(request);
 
+        var response = await _client.SendAsync(request);
+
+        // The dummy client returns the authorization header value if any
         var token = await response.Content.ReadAsStringAsync();
-
-        // the dummy client returns the authorization header
-        Assert.That(token, Is.EqualTo("Bearer " + authToken));
+        Assert.That(token, Is.EqualTo("Bearer " + _authToken));
     }
 
     [Test]
     public async Task HandlerDoesNotGetTokenWhenAnonymous()
     {
-        var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
-        var context = Substitute.For<HttpContext>();
-        var services = new ServiceCollection();
-        context.RequestServices.Returns(services.BuildServiceProvider());
-        httpContextAccessor.HttpContext.Returns(context);
-
-        var handler = new AuthHeaderHandler(
-            httpContextAccessor,
-            NullLogger<AuthHeaderHandler>.Instance,
-            Substitute.For<ICurrentUser>(),
-            Options.Create(new HelseIdWebKonfigurasjon()),
-            new BearerAuthorizationHeaderSetter());
-
-        handler.InnerHandler = new DummyInnerHandler();
-
-        var client = new HttpClient(handler);
         var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/");
         request.Options.TryAdd("Anonymous", "Anonymous");
-        var response = await client.SendAsync(request);
 
+        var response = await _client.SendAsync(request);
+
+        // The dummy client returns the authorization header value if any
         var token = await response.Content.ReadAsStringAsync();
-
         Assert.That(token, Is.EqualTo(""));
     }
 }
@@ -111,7 +98,7 @@ public class DummyInnerHandler : HttpClientHandler
 {
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        // Get any auth header, and use its value as the content of the request
+        // Get any auth header, and use its value as the content of the response
         var auth = request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value?.FirstOrDefault() ?? "";
         var response = new HttpResponseMessage() { Content = new StringContent(auth) };
         return Task.FromResult(response);
