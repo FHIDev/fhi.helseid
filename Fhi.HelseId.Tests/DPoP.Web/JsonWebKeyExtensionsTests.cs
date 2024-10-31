804c6d80e3d315408b273cd1565eb2a1d6035085
Martin Storø Nyfløtt <MartinStoro.Nyflott@fhi.no>
2024-10-31 14:08:20 +0100

    Fixed a bug with ClientSecret being broken with DPoP for KeyVault

---
+++ Fhi.HelseId.Tests/DPoP.Web/JsonWebKeyExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using Fhi.HelseId.Web.DPoP;
+using Microsoft.IdentityModel.Tokens;
+using NUnit.Framework;
+
+namespace Fhi.HelseId.Tests.DPoP.Web;
+
+public class JsonWebKeyExtensionsTests
+{
+    [Test]
+    public void KeyShouldBeCloned()
+    {
+        // Arrange
+        var rsaKey = new RsaSecurityKey(RSA.Create(2048));
+        var proofKey = JsonWebKeyConverter.ConvertFromSecurityKey(rsaKey);
+        
+        // Act
+        var key = proofKey.AsDPoPJwkSecret();
+        key.Kty = "test";
+
+        // Assert
+        Assert.That(key.Kty, Is.Not.EqualTo(proofKey.Kty));
+    }
+
+    [Test]
+    public void AlgIsSetToPS256ByDefault()
+    {
+        // Arrange
+        var rsaKey = new RsaSecurityKey(RSA.Create(2048));
+        var proofKey = JsonWebKeyConverter.ConvertFromSecurityKey(rsaKey);
+        proofKey.Alg = null;
+        
+        // Act
+        var key = proofKey.AsDPoPJwkSecret();
+
+        // Assert
+        Assert.That(key.Alg, Is.EqualTo("PS256"));
+    }
+}
---
+++ Fhi.HelseId.Web/DPoP/JsonWebKeyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Fhi.HelseId.Web.DPoP;
+
+public static class JsonWebKeyExtensions
+{
+    public static JsonWebKey AsDPoPJwkSecret(this JsonWebKey key)
+    {
+        var json = JsonSerializer.Serialize(key);
+        var clonedKey = JsonSerializer.Deserialize<JsonWebKey>(json)!;
+        clonedKey.Alg ??= "PS256";
+        
+        return clonedKey;
+    }
+}
