dbb454745c4f905ce3495baf8aa17f10d4d4b9b2
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2024-11-01 11:00:31 +0100

    Removed remnants of cache and obsolete error handling.

--- Fhi.HelseId.Web/Hpr/HprService.cs
+++ Fhi.HelseId.Web/Hpr/HprService.cs
@@ -1,7 +1,6 @@
 ﻿using Fhi.HelseId.Web.Hpr.Core;
 using Fhi.HelseId.Web.Services;
 using HprServiceReference;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -23,7 +22,6 @@ namespace Fhi.HelseId.Web.Hpr
         IHprService LeggTilGodkjenteHelsepersonellkategori(OId9060 ny);
         IHprService LeggTilGodkjenteHelsepersonellkategorier(IGodkjenteHprKategoriListe liste);
         bool ErGyldigForKategorier(Person person, params OId9060[] koder);
-        string LastErrorMessage { get; }
         List<OId9060> GodkjenteHelsepersonellkategorier { get; }
         Task<IEnumerable<OId9060>> HentGodkjenninger(string hprnummer);
         IEnumerable<OId9060> HentGodkjenninger(Person? person);
@@ -38,7 +36,6 @@ namespace Fhi.HelseId.Web.Hpr
         public List<OId9060> GodkjenteHelsepersonellkategorier { get; }
 
         const string HprnummerAdmin = "000000000";
-        public string LastErrorMessage { get; private set; } = "";
 
         public HprService(ICurrentUser currentUser, ILogger<HprService> logger)
         {
@@ -87,51 +84,16 @@ namespace Fhi.HelseId.Web.Hpr
             return filteredGodkjenninger.Any();
         }
 
-        public async Task<HprPerson?> HentPerson(string hprnummer)
+        public async Task<HprPerson> HentPerson(string hprnummer)
         {
-            var personFraRegister = await HentFraHprRegister(hprnummer);
-
-            var cacheTidISekunder = 600;
-            var cacheEntryOptions = new MemoryCacheEntryOptions()
-                .SetAbsoluteExpiration(TimeSpan.FromSeconds(cacheTidISekunder));
-
-            _logger.LogDebug("Person med Hpr-nummer {HprNummer} hentet fra register og lagt til i cache i {CacheTid} sekunder", hprnummer, cacheTidISekunder);
-
-            return personFraRegister;
-        }
-
-        private async Task<HprPerson?> HentFraHprRegister(string hprnummer)
-        {
-            try
+            var person = new HprPerson()
             {
-                var person = new HprPerson()
-                {
-                    HprNummer = hprnummer,
-                    ErHprGodkjent = _currentUser.ErHprGodkjent,
-                    HprGodkjenninger = _currentUser.HprGodkjenninger
-                };
+                HprNummer = hprnummer,
+                ErHprGodkjent = _currentUser.ErHprGodkjent,
+                HprGodkjenninger = _currentUser.HprGodkjenninger
+            };
 
-                return person;
-            }
-            catch (System.ServiceModel.CommunicationException e)
-            {
-                var msg = "CommunicationException i aksess til Hpr register. " + e;
-                LastErrorMessage = msg;
-                _logger.LogError(e, msg);
-                return null;
-            }
-#pragma warning disable 168
-#pragma warning disable CA1031 // Do not catch general exception types
-            catch (Exception e)
-#pragma warning restore 168
-            {
-                //Hvis ekstern service kaster exception returneres null. Eksemplvis mottar vi også en exception hvis fnr ikke finnes.
-                const string msg = "Feil i aksess til Hpr register. (Obs: Mottar også en exception hvis fnr ikke finnes)";
-                LastErrorMessage = msg;
-                _logger.LogError(e, msg);
-                return null;
-            }
-#pragma warning restore CA1031 // Do not catch general exception types
+            return person;
         }
 
         /// <summary>
