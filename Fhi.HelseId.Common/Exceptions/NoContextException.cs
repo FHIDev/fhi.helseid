b4ea5aa54ab9de0836ea791305ccdb7625ff4c45
Geir Marius Gjul (Innleid) <Geir.Marius.Gjul@fhi.no>
2024-11-27 16:40:50 +0100

    Fixes: A single-line comment within C# code is followed by a blank line The C# comment does not contain any comment text

--- Fhi.HelseId.Common/Exceptions/NoContextException.cs
+++ Fhi.HelseId.Common/Exceptions/NoContextException.cs
@@ -4,13 +4,10 @@ namespace Fhi.HelseId.Common.Exceptions;
 
 public class NoContextException : Exception
 {
-    //
     // For guidelines regarding the creation of new exception types, see
     //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
     // and
     //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
-    //
-
     public NoContextException()
     {
     }
@@ -22,5 +19,4 @@ public class NoContextException : Exception
     public NoContextException(string message, Exception inner) : base(message, inner)
     {
     }
-
 }
