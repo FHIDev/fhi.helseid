141187012bd43ca9bbb5a7c09ac5c1e61d8c08e9
Jenny Beate Hougen <Jenny.Beate.Hougen@fhi.no>
2024-12-02 17:42:01 +0100

    Add samples of hosting models in tests

--- Fhi.HelseId.Integration.Tests/TestFramework/TestWebApplicationFactory.cs
+++ Fhi.HelseId.Integration.Tests/TestFramework/TestWebApplicationFactory.cs
@@ -1,41 +1,58 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Fhi.HelseId.Integration.Tests.TestFramework
+public class TestWebApplicationFactory : WebApplicationFactory<Program>
 {
-    public class TestWebApplicationFactory : WebApplicationFactory<Program>
+    private IConfiguration? _configuration { get; }
+    private Action<IServiceCollection> _serviceCollection { get; }
+    private Action<IApplicationBuilder>? _appBuilder { get; }
+
+    public TestWebApplicationFactory(IConfiguration configuration, Action<IServiceCollection> services,
+        Action<IApplicationBuilder>? appBuilder = null)
     {
-        private IConfiguration? _configuration { get; }
-        private Action<IServiceCollection> _serviceCollection { get; }
+        _configuration = configuration;
+        _serviceCollection = services;
+        _appBuilder = appBuilder;
 
-        public TestWebApplicationFactory(IConfiguration configuration, Action<IServiceCollection> services)
-        {
-            _configuration = configuration;
-            _serviceCollection = services;
-        }
+    }
 
-        public TestWebApplicationFactory(Action<IServiceCollection> services)
-        {
-            _serviceCollection = services;
-        }
+    public TestWebApplicationFactory(Action<IServiceCollection> services)
+    {
+        _serviceCollection = services;
+    }
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        if (_configuration is not null)
         {
-            if (_configuration is not null)
+            builder.ConfigureAppConfiguration((context, config) =>
             {
-                builder.ConfigureAppConfiguration((context, config) =>
-                {
-                    config.AddConfiguration(_configuration);
-                });
-            }
-            builder.ConfigureTestServices(_serviceCollection.Invoke);
+                config.AddConfiguration(_configuration);
+            });
         }
+
+        builder.ConfigureTestServices(_serviceCollection.Invoke);
+
+        builder.Configure(app =>
+        {
+            _appBuilder?.Invoke(app);
+        });
+
+    }
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Add or replace services for testing
+        });
+
+
+        return base.CreateHost(builder);
     }
 }
-
-
-
-
---
+++ Fhi.HelseId.Integration.Tests/tmp/TestHostBuilder.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Fhi.HelseId.Integration.Tests.TestFramework
+{
+    internal static class TestHostBuilder
+    {
+        internal static Action<IWebHostBuilder> Configure(IConfiguration configuration, Action<IServiceCollection> serviceCollection, Action<IApplicationBuilder> appBuilder)
+        {
+            Action<IWebHostBuilder> configure = builder =>
+            {
+                var webHostBuilder = builder.UseTestServer();
+                webHostBuilder.AddServiceCollection(serviceCollection);
+                webHostBuilder.AddApp(appBuilder);
+                webHostBuilder.AddAppConfiguration(configuration);
+            };
+
+            return configure;
+        }
+
+        internal static Action<IWebHostBuilder> ConfigureWithStartup<T>(IConfiguration configuration, Action<IServiceCollection> serviceCollection)
+            where T : class
+        {
+            Action<IWebHostBuilder> configure = builder =>
+            {
+                var webHostBuilder = builder.UseTestServer();
+                webHostBuilder.UseStartup<T>();
+                webHostBuilder.AddServiceCollection(serviceCollection);
+                webHostBuilder.AddAppConfiguration(configuration);
+            };
+
+            return configure;
+        }
+
+    }
+
+    internal static class IWebHostBuilderExtensions
+    {
+        internal static TestServer CreateTestServer(this Action<IWebHostBuilder> conf)
+        {
+            var builder = new HostBuilder().ConfigureWebHost(conf);
+            var host = builder.Build();
+            host.Start();
+
+            return host.GetTestServer();
+        }
+
+        internal static IWebHostBuilder AddServiceCollection(this IWebHostBuilder builder, Action<IServiceCollection> serviceCollection)
+        {
+            return builder.ConfigureServices(serviceCollection.Invoke);
+        }
+
+        internal static IWebHostBuilder AddApp(this IWebHostBuilder builder, Action<IApplicationBuilder> appBuilder)
+        {
+            return builder.Configure(appBuilder.Invoke);
+        }
+
+        internal static IWebHostBuilder AddAppConfiguration(this IWebHostBuilder builder, IConfiguration configuration)
+        {
+            return builder.ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddConfiguration(configuration);
+            });
+        }
+    }
+
+
+
+
+
+}
