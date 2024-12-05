c1baa9b8560a4094ec9232d26bc8e2061910a59e
Jenny Beate Hougen <Jenny.Beate.Hougen@fhi.no>
2024-12-05 14:00:31 +0100

    remove unused nuget

--- Fhi.HelseId.Samples/Authentication/FakeAuthenticationSamples.cs
+++ Fhi.HelseId.Samples/Authentication/FakeAuthenticationSamples.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using Fhi.HelseId.Common.Identity;
 using Fhi.HelseId.Integration.Tests.TestFramework;
 using Fhi.HelseId.Web;
